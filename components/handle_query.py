3de2df4a3985ae6114b3bfc09528c0c09d5c0f0b
lifewjola <anjolaajayi3@gmail.com>
2024-09-26 23:07:05 +0100

    fallback message for current affairs

--- components/handle_query.py
+++ components/handle_query.py
@@ -8,6 +8,7 @@ def get_intent(query):
     lecturer_keywords = ["who", "name", "lecturer", "lecturer's" "phone number", "no", "phone", "number", "office"]
     doc_link_keywords = ["past questions", "pstq", "study materials", "flashcards", "studysmarter",
                          "study smarter", "slides", "slide", "pdf"]
+    unknown_keywords = ["class"]
 
     # Check for keywords in the query
     query_lower = query.lower()
@@ -15,6 +16,8 @@ def get_intent(query):
         return "lecturer"
     elif any(keyword in query_lower for keyword in doc_link_keywords):
         return "doc_link"
+    elif any(keyword in query_lower for keyword in unknown_keywords):
+        return "unknown"
     else:
         return "general"
 
@@ -26,6 +29,8 @@ def handle_query(query):
         response = answer_lecturer_query(query)
     elif intent == 'doc_link':
         response =  answer_doc_link_query(query)
+    elif intent == 'unknown':
+        response =  "I'm sorry but I can't help with that. Please reach out to Anji, the human course rep, or any of the class excos."
     else:
         response = answer_general_query(query)
     for word in response.split():
